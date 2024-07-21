60f4b275441ebc9e6834c537205394233fc3771a
Dhruv Kuchhal <ddkuchhal@gmail.com>
2024-07-21 21:08:01 +0530

    feat: create 121_Best_Time_to_Buy_and_Sell_Stock.cpp

---
+++ Day 7/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int profit = 0;
+        int mini = prices[0];
+        int n = prices.size();
+        for (int i = 0; i < n; i++)
+        {
+            int cost = prices[i] - mini;
+            profit = max(profit, cost);
+            mini = min(prices[i], mini);
+        }
+        return profit;
+    }
+};
